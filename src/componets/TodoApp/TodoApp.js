12db483a45be32b21c9ed0918cdddd0434b34aa7
IsmailAvayil <ismailavayil@gmail.com>
2024-01-19 22:34:22 +0530

    first commit

--- src/componets/TodoApp/TodoApp.js
+++ src/componets/TodoApp/TodoApp.js
@@ -1,54 +1,69 @@
-
-import React,{ useContext } from "react";
+import React, { useContext } from "react";
 import "./TodoApp.css";
 import { AppContext } from "../../ReactContext/AppContext";
 
 function TodoApp() {
-  const {handleChangeDetails,handleChangeTitle,userForm,userData,handleSubmit,setUserData} =useContext(AppContext)
-  const deleteItem=(item)=>{
-    const d=userData.filter((obj,index)=>
-      index !== item
-    )
-    setUserData(d)
-
-  }
+  const {
+    handleChangeDetails,
+    handleChangeTitle,
+    userForm,
+    userData,
+    handleSubmit,
+    setUserData,
+  } = useContext(AppContext);
+  const deleteItem = (item) => {
+    const d = userData.filter((obj, index) => index !== item);
+    setUserData(d);
+  };
   return (
     <div className="todo-container">
-      
-      <form className="user-input" onSubmit={handleSubmit} >
-      <h1>TodoApp</h1>
+      <form className="user-input" onSubmit={handleSubmit}>
+        <h1>TodoApp</h1>
         <div>
-          <input type="text" placeholder="Enter the title" onChange={handleChangeTitle} name="title" value={userForm.title}></input>
-        
+          <input
+            type="text"
+            placeholder="Enter the title"
+            onChange={handleChangeTitle}
+            name="title"
+            value={userForm.title}
+          ></input>
         </div>
         <div>
-          <textarea type="text" placeholder="Enter the Detials" onChange={handleChangeDetails} name="details" value={userForm.details}></textarea>
-        
+          <textarea
+            type="text"
+            placeholder="Enter the Detials"
+            onChange={handleChangeDetails}
+            name="details"
+            value={userForm.details}
+          ></textarea>
+        </div>
+        <div>
+    
+        <button type="submit" className="ui green button">Add</button>
+      
         </div>
 
-        <button type="submit" >Add</button>
+      
       </form>
-      {userData && userData.map((obj,index)=>(
-        <ul>
-          <li>
-            <div>
-            <h3>{obj.title}</h3><i className="fa-solid fa-trash" onClick={()=>deleteItem(index)}></i>
-
-           
-          <i class="fa-solid fa-check"></i>
-          </div>
-          <p>{obj.details}</p>
-          
-          </li>
-          
-          
-        
-        </ul>
-  
-
-      ))}
-
-      
+      {userData &&
+        userData.map((obj, index) => (
+          <ul>
+            <li>
+              <div>
+                <h3>{obj.title}</h3>
+                <p>{obj.details}</p>
+              </div>
+              <div className="icons">
+                <i
+                  className="fa-solid fa-trash deleteicon"
+                  onClick={() => deleteItem(index)}
+                ></i>
+                <i class="fa-solid fa-check tickicon"></i>
+                <p>completed</p>
+              </div>
+            </li>
+          </ul>
+        ))}
     </div>
   );
 }
